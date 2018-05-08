3d4b87535a68d79321cc863eecb1fd17082b5ebb
chenshunyi <125@qq.com>
2018-05-08 15:32:48 +0800

    新增model

---
+++ XFComposition/MyWritingDetailModel.h
@@ -0,0 +1,13 @@
+//
+//  MyWritingDetailModel.h
+//  XFComposition
+//
+//  Created by chenshunyi on 2018/5/8.
+//  Copyright © 2018年 周凤喜. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyWritingDetailModel : NSObject
+
+@end
---
+++ XFComposition/MyWritingDetailModel.m
@@ -0,0 +1,13 @@
+//
+//  MyWritingDetailModel.m
+//  XFComposition
+//
+//  Created by chenshunyi on 2018/5/8.
+//  Copyright © 2018年 周凤喜. All rights reserved.
+//
+
+#import "MyWritingDetailModel.h"
+
+@implementation MyWritingDetailModel
+
+@end
