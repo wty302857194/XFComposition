a27d249eee81d79e1ebdfca2dc29c04ce2194126
wbb <302857194@qq.com>
2018-05-11 16:25:01 +0800

    旁批

--- XFComposition/TYCorrectViewController.m
+++ XFComposition/TYCorrectViewController.m
@@ -9,51 +9,17 @@
 #import "TYCorrectViewController.h"
 
 @interface TYCorrectViewController ()<UIScrollViewDelegate>
-@property (nonatomic,strong)UIScrollView *scorllView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *leftView;
+@property (weak, nonatomic) IBOutlet UIView *rightView;
 
 @end
 
 @implementation TYCorrectViewController
 
--(UIScrollView *)scorllView{
-    if (!_scorllView) {
-        _scorllView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, WidthFrame, HeightFrame)];
-        _scorllView.delegate = self;
-        _scorllView.contentSize = CGSizeMake(WidthFrame*3/2, HeightFrame-64);
-        _scorllView.pagingEnabled = YES;//设置整屏滚动
-        _scorllView.bounces = YES;//设置边缘无弹跳
-        //添加点按击手势监听器
-//        UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapUiscrollView:)];
-//        //设置手势属性
-//        tapGesture.delegate = self;
-//        tapGesture.numberOfTapsRequired=1;//设置点按次数，默认为1，注意在iOS中很少用双击操作
-//        tapGesture.numberOfTouchesRequired=1;//点按的手指数
-//        [_scorllView addGestureRecognizer:tapGesture];
-        _scorllView.delaysContentTouches = NO;
-//        UIButton *addBt = [UIButton buttonWithType:UIButtonTypeCustom];
-//        [addBt setTitle:@"添加" forState:UIControlStateNormal];
-//        addBt.backgroundColor = [UIColor colorWithHexString:@"348DCC"];
-//        addBt.frame = CGRectMake(WidthFrame+WidthFrame/8, HeightFrame-40-64, WidthFrame/4, 30);
-//        addBt.layer.cornerRadius = 4;
-//        [addBt addTarget:self action:@selector(creatView) forControlEvents:UIControlEventTouchUpInside];
-//        addBt.layer.masksToBounds = YES;
-//        [_scorllView addSubview:addBt];
-//
-//        UIButton *addBt1 = [UIButton buttonWithType:UIButtonTypeCustom];
-//        [addBt1 setTitle:@"编辑图片" forState:UIControlStateNormal];
-//        addBt1.backgroundColor = [UIColor colorWithHexString:@"348DCC"];
-//        addBt1.frame = CGRectMake(WidthFrame+WidthFrame/8, HeightFrame-40-64-40, WidthFrame/4, 30);
-//        addBt1.layer.cornerRadius = 4;
-//        [addBt1 addTarget:self action:@selector(creatView1) forControlEvents:UIControlEventTouchUpInside];
-//        addBt1.layer.masksToBounds = YES;
-//        [_scorllView addSubview:addBt1];
-        
-    }
-    return _scorllView;
-}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    _scrollView.contentSize = CGSizeMake(WidthFrame*3/2, HeightFrame);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -61,14 +27,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    int index = scrollView.contentOffset.x/kScreenWidth;
+    if (index == 1) {
+        [_scrollView setContentOffset:CGPointMake(index*kScreenWidth, _scrollView.contentOffset.y)];
+    }
 }
-*/
 
 @end
---
+++ XFComposition/图片处理器/TYImageEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  TYImageEditViewController.h
+//  MyPracticeViewController
+//
+//  Created by wbb on 2018/5/11.
+//  Copyright © 2018年 cjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYImageEditViewController : UIViewController
+
+@end
