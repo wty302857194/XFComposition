d88a387fff3882eda1e279f41920ae308bf39b5d
wbb <302857194@qq.com>
2018-05-15 17:17:07 +0800

    代码 提交

--- XFComposition/PicListViewController.m
+++ XFComposition/PicListViewController.m
@@ -175,7 +175,7 @@
    
     GetWritePicModel *model = self.picArray[indexPath.row];
     
-    TYCorrectViewController *vc = [[TYCorrectViewController alloc]init];
+//    TYCorrectViewController *vc = [[TYCorrectViewController alloc]init];
 //    NSString *pic = [NSString stringWithFormat:@"%@",model.FixPicUrl];
 //    if (pic.length > 3){
 //       vc.PicUrl = model.FixPicUrl;
@@ -185,8 +185,8 @@
 //
 //        vc.PicID = model.ID;
 //
-    vc.picModel = model;
-    [self.navigationController pushViewController:vc animated:YES];
+//    vc.picModel = model;
+//    [self.navigationController pushViewController:vc animated:YES];
     TYCorrectViewController *vc = [[TYCorrectViewController alloc] init];
     NSString *pic = [NSString stringWithFormat:@"%@",model.FixPicUrl];
     if (pic.length > 3){
