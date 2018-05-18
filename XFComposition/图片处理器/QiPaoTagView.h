d4b9c6831e5e8038ffcf9da972463aeff38945f4
wbb <302857194@qq.com>
2018-05-18 11:00:40 +0800

    气泡接口  内容提交 修改

--- XFComposition/图片处理器/QiPaoTagView.h
+++ XFComposition/图片处理器/QiPaoTagView.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 typedef void(^ContentStrBlock) (NSString *contentStr);
+typedef void(^LocationBlock) (CGRect frame);
 @interface QiPaoTagView : UIView
 @property (nonatomic,copy) ContentStrBlock contentStrBlock;//编辑的信息
+@property (nonatomic,copy) LocationBlock locationBlock;//编辑的信息
+
+@property (nonatomic,copy) NSString *time;
 @property (nonatomic,strong) FSTextView *textView ;
 @end
